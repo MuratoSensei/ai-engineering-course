a7fc7181217a7deef2cea1493f6adea0096fea06
MuratoSensei <muratosensei@gmail.com>
2026-05-30 19:03:12 +0400

    OOP project

--- Book.py
+++ Book.py
@@ -3,4 +3,18 @@ class Book:
         self.title = title
         self.author = author
         self.release_year = release_year
-        self.pages = pages
+        self.__pages = pages
+    
+    def get_pages(self):
+        return self.__pages
+    
+    def __str__(self)
+        return f"'{self.title}' by {self.author} ({self.release_year}) - {self.__pages} pages"
+    
+class AudioBook(Book):
+    def __init__(self, title, author, release_year, duration):
+        super().__init__(title, author, release_year, pages=0)
+        self.duration = duration
+
+    def __str__(self):
+        return f"[AudioBook: '{self.title}' by {self.author} ({self.release_year}) - Duration:{self.duration} min]"
